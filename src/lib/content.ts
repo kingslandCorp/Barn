1a1703b413f9699b9da8b38a1fb77267d5756c03
kingslandCorp <p.g.davis@outlook.com>
2026-07-30 15:31:03 +0100

    Update content.ts

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -265,8 +265,7 @@ export const what3words = {
 };
 
 export const mapEmbedSrc =
-  'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d31585.782690926404!2d-3.4426292770617644!3d51.43566713670784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2suk!4v1785418420983!5m2!1sen!2suk';
-
+  'https://www.google.com/maps?q=51.43566713670784,-3.4426292770617644&z=13&output=embed';
 export const parkingInfo = {
   title: 'Parking',
   description:
